e1d4e501bdb4ed329833f8536c4f85b295254439
Cesar Macedo <czar92981@gmail.com>
2024-04-02 18:38:27 -0300

    Scrabble - functional

--- cs50-x/week-2/ps1-scrabble/scrabble.c
+++ cs50-x/week-2/ps1-scrabble/scrabble.c
@@ -1,13 +1,19 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+
+int score(string s);
+
+int pointTable[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void){
-    char pointTable = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
+
     int p1 = score(player1);
     int p2 = score(player2);
-
+    
     if(p1 > p2){
         printf("Player 1 wins!");
     }else if(p1 < p2){
@@ -18,6 +24,13 @@ int main(void){
 }
 
 int score(string s){
+    int points = 0;
 
-    return 0;
+    for(int i = 0, lenght = strlen(s); i < lenght; i++){
+        if(isalpha(s[i]) != 0){
+            points += pointTable[toupper(s[i]) - 'A'];
+        }
+    }
+
+    return points;
 }
