a606642b0fa4fc781328c151ecd8b42a14d15180
Cesar Macedo <czar92981@gmail.com>
2024-04-03 16:45:37 -0300

    Substitution - catches all cases

--- cs50-x/week-2/ps4-substitution/substitution.c
+++ cs50-x/week-2/ps4-substitution/substitution.c
@@ -2,17 +2,14 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 bool checkKey(string key);
+char substitute(char c, string key);
 
 int main(int argc, string argv[]){
     if(argc != 2){
         printf("Usage: ./substitution key\n");
-        return 1;
-    }
-
-    if(strlen(argv[1]) != 26){
-        printf("Key must contain 26 characters\n");
         return 1;
     }
 
@@ -21,32 +18,46 @@ int main(int argc, string argv[]){
         return 1;
     }
 
-    
+    string plaintext = get_string("Plaintext:  ");
+
+    printf("ciphertext: ");
+
+    for(int i = 0, length = strlen(plaintext); i < length; i++){
+        printf("%c", substitute(plaintext[i], argv[1]));
+    }
+
+    printf("\n");
+    return 0;
 }
 
 bool checkKey(string key){
-    int sum = 0, reference = 0;
-    char c = 'A';
+    if(strlen(key) != 26){
+        return false;
+    }
+
+    for(int i = 0, length = strlen(key); i < length; i++){
+        key[i] = toupper(key[i]);
+    }
 
     for(int i = 0, length = strlen(key); i < length; i++){
         if(!isalpha(key[i])){
             return false;
         }
 
-        reference += c - 'A' + 1;
-        c++;
-
-        if(isupper(key[i])){
-            sum += key[i] - 'A' + 1;
-        }else{
-            sum += key[i] - 'a' + 1;
+        if(index(key, key[i]) != rindex(key, key[i])){
+            return false;
         }
     }
 
-    if(sum != reference){
-        return false;
-    }else{
-        return true;
-    }
+    return true;
 }
 
+char substitute(char c, string key){
+    if(isupper(c)){
+        return toupper(key[c - 'A']);
+    }else if(islower(c)){
+        return tolower(key[c - 'a']);
+    }else{
+        return c;
+    }
+}
