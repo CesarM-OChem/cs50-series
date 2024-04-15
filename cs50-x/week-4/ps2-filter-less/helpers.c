193a761cbea761968865a3b4bfc437ea12d93975
Cesar_Macedo <czar92981@gmail.com>
2024-04-15 12:06:32 -0300

    Filter-less applies grayscale

--- cs50-x/week-4/ps2-filter-less/helpers.c
+++ cs50-x/week-4/ps2-filter-less/helpers.c
@@ -4,6 +4,26 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            float average = 0;
+            
+            average += image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+            average /= 3;
+
+            if(average >=255){
+                image[i][j].rgbtRed = 255;
+                image[i][j].rgbtGreen = 255;
+                image[i][j].rgbtBlue = 255;
+            }else{
+                image[i][j].rgbtRed = (int) round(average);
+                image[i][j].rgbtGreen = (int) round(average);
+                image[i][j].rgbtBlue = (int) round(average);
+            }
+        }
+        
+    }
+    
     return;
 }
 
