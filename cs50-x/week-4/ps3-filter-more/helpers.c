7da3f593766f7d2e198f149816ef4a846f7dff27
Cesar_Macedo <czar92981@gmail.com>
2024-04-16 13:16:40 -0300

    Filter-more - applies reflect

--- cs50-x/week-4/ps3-filter-more/helpers.c
+++ cs50-x/week-4/ps3-filter-more/helpers.c
@@ -29,6 +29,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width / 2; j++){
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = tmp;
+        }
+        
+    }
+    
     return;
 }
 
