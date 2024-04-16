72d9cda328cc4ad548ef41dd0d2a3817cd57e945
Cesar_Macedo <czar92981@gmail.com>
2024-04-16 13:14:13 -0300

    Filter-more - applies grayscale

--- cs50-x/week-4/ps3-filter-more/helpers.c
+++ cs50-x/week-4/ps3-filter-more/helpers.c
@@ -1,8 +1,28 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            float average = 0;
+            average += image[i][j].rgbtRed;
+            average += image[i][j].rgbtGreen;
+            average += image[i][j].rgbtBlue;
+            average = round(average / 3);
+
+            if(average > 255){
+                average = 255;
+            }
+
+            image[i][j].rgbtRed = (int)average;
+            image[i][j].rgbtGreen = (int)average;
+            image[i][j].rgbtBlue = (int)average;
+        }
+        
+    }
+    
     return;
 }
 
