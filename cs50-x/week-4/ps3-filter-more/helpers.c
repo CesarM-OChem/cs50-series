66198ed16f57929b79c8858c74344e8c7f4f974a
Cesar_Macedo <czar92981@gmail.com>
2024-04-16 13:39:09 -0300

    Filter-more - applies blur

--- cs50-x/week-4/ps3-filter-more/helpers.c
+++ cs50-x/week-4/ps3-filter-more/helpers.c
@@ -1,6 +1,9 @@
 #include "helpers.h"
 #include <math.h>
 
+// Helper function prototype
+void sumRGB(float array[], RGBTRIPLE pixel);
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -44,11 +47,100 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            copy[i][j] = image[i][j];
+        }
+    }
+    
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            int counter = 1;
+            float sum[3] = {0, 0, 0}; // Array of RGB sum values
+
+            sumRGB(sum, copy[i][j]);
+
+            // Checks for upper row and top left and right pixels
+            if(i > 0){
+                sumRGB(sum, copy[i - 1][j]);
+                counter++;
+
+                if(j > 0){
+                    sumRGB(sum, copy[i - 1][j - 1]);
+                    counter++;
+                }
+
+                if(j < width - 1){
+                    sumRGB(sum, copy[i - 1][j + 1]);
+                    counter++;
+                }
+            }
+
+            // Checks for lower row and bottom left and right pixels
+            if(i < height - 1){
+                sumRGB(sum, copy[i + 1][j]);
+                counter++;
+
+                if(j > 0){
+                    sumRGB(sum, copy[i + 1][j - 1]);
+                    counter++;
+                }
+
+                if(j < width - 1){
+                    sumRGB(sum, copy[i + 1][j + 1]);
+                    counter++;
+                }
+            }
+
+            // Checks for left pixel
+            if(j > 0){
+                sumRGB(sum, copy[i][j - 1]);
+                counter++;
+            }
+
+            // Checks for right pixel
+            if(j < width - 1){
+                sumRGB(sum, copy[i][j + 1]);
+                counter++;
+            }
+
+            sum[0] = round(sum[0] / counter);
+            sum[1] = round(sum[1] / counter);
+            sum[2] = round(sum[2] / counter);
+
+            if(sum[0] > 255){
+                sum[0] = 255;
+            }
+
+            if(sum[1] > 255){
+                sum[1] = 255;
+            }
+
+            if(sum[2] > 255){
+                sum[2] = 255;
+            }
+
+            image[i][j].rgbtRed = (int)sum[0];
+            image[i][j].rgbtGreen = (int)sum[1];
+            image[i][j].rgbtBlue = (int)sum[2];
+        }
+    }
+    
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    return;
+}
+
+// Sums the RGB values to the sum array
+void sumRGB(float array[], RGBTRIPLE pixel){
+    array[0] += pixel.rgbtRed;
+    array[1] += pixel.rgbtGreen;
+    array[2] += pixel.rgbtBlue;
+    
     return;
 }
