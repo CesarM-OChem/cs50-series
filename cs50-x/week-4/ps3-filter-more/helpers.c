2e1132d895c414b26d27a7edfc77a0cf20ace1ac
Cesar_Macedo <czar92981@gmail.com>
2024-04-17 10:36:36 -0300

    Filter-more - applies edge filter

--- cs50-x/week-4/ps3-filter-more/helpers.c
+++ cs50-x/week-4/ps3-filter-more/helpers.c
@@ -3,6 +3,7 @@
 
 // Helper function prototype
 void sumRGB(float array[], RGBTRIPLE pixel);
+void weightSum(int weight, float array[], RGBTRIPLE pixel);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -133,6 +134,70 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height + 2][width + 2]; //Copies the image array adding a black edge and reduce the number of conditionals
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            copy[i + 1][j + 1] = image[i][j];
+        }
+    }
+
+    RGBTRIPLE black = {0, 0, 0};
+
+    for (int i = 0; i < height + 2; i++){
+        copy[i][0] = black;
+        copy[i][width + 1] = black;
+    }
+    
+    for (int j = 0; j < width + 2; j++){
+        copy[0][j] = black;
+        copy[height + 1][j] = black;
+    }
+    
+    // Iterates over the pixels outside of the black edge
+    for (int i = 1; i < height + 1; i++){
+        for (int j = 1; j < width + 1; j++){
+            float gx[] = {0, 0, 0}, gy[] = {0, 0, 0}; // RGB arrays
+
+            // Gx:
+            weightSum(-1, gx, copy[i - 1][j - 1]);
+            weightSum(-2, gx, copy[i][j - 1]);
+            weightSum(-1, gx, copy[i + 1][j - 1]);
+            weightSum(1, gx, copy[i - 1][j + 1]);
+            weightSum(2, gx, copy[i][j + 1]);
+            weightSum(1, gx, copy[i + 1][j + 1]);
+
+            // Gy:
+            weightSum(-1, gy, copy[i - 1][j - 1]);
+            weightSum(-2, gy, copy[i - 1][j]);
+            weightSum(-1, gy, copy[i - 1][j + 1]);
+            weightSum(1, gy, copy[i + 1][j - 1]);
+            weightSum(2, gy, copy[i + 1][j]);
+            weightSum(1, gy, copy[i + 1][j + 1]);
+
+            // Calculates vectorial sum of Gx and Gy for each RGB channel
+            float norm[3];
+            norm[0] = round(sqrt(pow(gx[0], 2) + pow(gy[0], 2)));
+            norm[1] = round(sqrt(pow(gx[1], 2) + pow(gy[1], 2)));
+            norm[2] = round(sqrt(pow(gx[2], 2) + pow(gy[2], 2)));
+
+            if(norm[0] > 255){
+                norm[0] = 255;
+            }
+
+            if(norm[1] > 255){
+                norm[1] = 255;
+            }
+
+            if(norm[2] > 255){
+                norm[2] = 255;
+            }
+
+            image[i - 1][j - 1].rgbtRed = norm[0];
+            image[i - 1][j - 1].rgbtGreen = norm[1];
+            image[i - 1][j - 1].rgbtBlue = norm[2];
+        }
+    }
+    
     return;
 }
 
@@ -142,5 +207,14 @@ void sumRGB(float array[], RGBTRIPLE pixel){
     array[1] += pixel.rgbtGreen;
     array[2] += pixel.rgbtBlue;
     
+    return;
+}
+
+// Sums the weighted RGB values based on the Kernel
+void weightSum(int weight, float array[], RGBTRIPLE pixel){
+    array[0] += weight * pixel.rgbtRed;
+    array[1] += weight * pixel.rgbtGreen;
+    array[2] += weight * pixel.rgbtBlue;
+
     return;
 }
